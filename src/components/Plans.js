7dd393412e31a7ff49c9c5f54b892d4aef02e00d
marco <artofmarco@gmail.com>
2017-06-29 20:21:31 -0700

    login done marco

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import photo from '../assets/images/cafe2.jpg';
 import { connect } from 'react-redux';
-
+import './Plans.scss';
 
 
 function mapStateToProps(state) {
@@ -21,71 +21,71 @@ export class Plans extends React.Component {
     return (
     <div className="row">
 
-        <div className="col s12 m6 xl4">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">Package Silver</span>
+        <div className="col s12 m6 l4 xl4">
+          <div className="card bronze">
+            <div className="card-content ">
+              <span className="card-title">Package Bronze</span>
                <h2>
                 <em>$1700</em>
               </h2>
               <ul>
-              <li>Driver license</li>
-              <li>Bank Account</li>
-              <li>Cell phone</li>
+                <li>Driver license</li>
+                <li>Bank Account</li>
+                <li>Cell phone</li>
               </ul>
             </div>
             <div className="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
+              <a className="btn themeButton" href="#">Select</a>
+             
             </div>
           </div>
         </div>  
         
     
-        <div className="col s12 m6 xl4">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">Package Gold</span>
+        <div className="col s12 m6 l4 xl4">
+          <div className="card silver">
+            <div className="card-content ">
+              <span className="card-title">Package Silver</span>
               <h2>
                   <em>$3000</em>
                 </h2>
                 <ul>
-                <li>Driver license</li>
-                <li>Bank Account</li>
-                <li>Cell phone</li>
-                <li>Apartment</li>
+                  <li>Driver license</li>
+                  <li>Bank Account</li>
+                  <li>Cell phone</li>
+                  <li>Apartment</li>
                 
                 </ul>
             </div>
             <div className="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
+              <a className="btn themeButton" href="#">Select</a>
+              
             </div>
           </div>
         </div>
     
-        <div className="col s12 m6 xl4">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">Package Platinum</span>
+        <div className="col s12 m6 l4 xl4">
+          <div className="card gold">
+            <div className="card-content ">
+              <span className="card-title">Package Gold</span>
               
               <h2>
                   <em>$4000</em>
                 </h2>
                 <ul>
-                <li>Driver license</li>
-                <li>Bank Account</li>
-                <li>Cell phone</li>
-                <li>Apartment</li>
-                <li>School</li>
-                <li>Citizenship</li>
-                <li>Vehicle</li>
+                  <li>Driver license</li>
+                  <li>Bank Account</li>
+                  <li>Cell phone</li>
+                  <li>Apartment</li>
+                  <li>School</li>
+                  <li>Citizenship</li>
+                  <li>Vehicle</li>
                 
                 </ul>
             </div>
             <div className="card-action">
-              <a href="#">This is a link</a>
-              <a href="#">This is a link</a>
+              <a className="btn themeButton" href="#">Select</a>
+              
             </div>
           </div>
        </div>
