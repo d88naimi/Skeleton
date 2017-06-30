1914cf67769b4549c111943520e4f773ae808cf3
HYUNGWU PAE <monad98@naver.com>
2017-06-29 22:32:18 -0700

    agent reducer actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,19 +2,22 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import * as fromAuth from './auth';
 import * as fromLang from './lang';
-
-// import { createSelector } from 'reselect'
-
+import * as fromAgent from './agent';
+import { createSelector } from 'reselect'
+import getAgentList from './agent'
 /**
  * root reduces
  */
 export default combineReducers({
   auth: fromAuth.reducer,
   routing: routerReducer,
-  lang: fromLang.reducer
+  lang: fromLang.reducer,
+  agents: fromAgent.reducer
 });
 
 
 /**
  * selectors
  */
+const getAgentState = state => state.agents;
+const getAgentList = createSelector(getAgentState, getAgentList);
