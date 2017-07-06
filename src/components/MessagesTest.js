c718c5742f48daaf639fe416e14e3f2a1a058e40
stephen <stephenmurena@gmail.com>
2017-07-05 18:07:22 -0700

    push to merge master

--- src/components/MessagesTest.js
+++ src/components/MessagesTest.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Route } from 'react-router';
 import ChatBox from "./ChatBox";
 import { Link } from 'react-router-dom'
+import "./Messages.scss";
 
 
 class MessagesTest extends React.Component {
@@ -12,29 +13,41 @@ class MessagesTest extends React.Component {
     console.log(match);
     const ids = Object.keys(messages);
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col s5">
-            <ul className="collection">
-              { ids.map((id, idx) =>
-                (<li key={idx} className="collection-item" style={{marginBottom: '3px'}}>
-                  <Link to={match.path + '/' + id}><div className="row"  style={{marginBottom: 0}}>
-                    <div className="col s2">
-                      <img src={messages[id][0].to._id === user._id ? messages[id][0].from.photoURL : messages[id][0].to.photoURL} className="circle responsive-img"/>
-                    </div>
-                    <div className="col s10">
-                      <span><strong>{messages[id][0].to._id === user._id ? messages[id][0].from.name : messages[id][0].to.name}</strong></span>
-                      <div>{messages[id][messages[id].length -1].text}</div>
-                    </div>
-                  </div></Link>
-                </li>)
-              )}
-            </ul>
+      <div className="container grey lighten-5">
+        <div id="messagingContainer" className="row">
+          <section className="col s5">
+          <div className="row">
+              <div className="col s12 m12">
+                <div className="card row">
+                  <h3 className="col s8">Messaging</h3>
+                  <a className="col s4" href="#"><i className="medium material-icons">chat_bubble_outline</i></a>
+                </div>
+              </div>
+              <div className="col s12 m12">
+                <div className="card row">
+                  <ul className="collection">
+                    { ids.map((id, idx) =>
+                      (<li key={idx} className="collection-item" style={{marginBottom: '3px'}}>
+                        <Link to={match.path + '/' + id}><div className="row"  style={{marginBottom: 0}}>
+                          <div className="col s2">
+                            <img src={messages[id][0].to._id === user._id ? messages[id][0].from.photoURL : messages[id][0].to.photoURL} className="circle responsive-img"/>
+                          </div>
+                          <div className="col s10">
+                            <span><strong>{messages[id][0].to._id === user._id ? messages[id][0].from.name : messages[id][0].to.name}</strong></span>
+                            <div>{messages[id][messages[id].length -1].text}</div>
+                          </div>
+                        </div></Link>
+                      </li>)
+                    )}
+                  </ul>
+                </div>
+              </div>
           </div>
+          </section>
 
-          <div className="col s7">
+          <section className="col s7">
             <Route path={match.path + '/:opponent'} component={ChatBox} />
-          </div>
+          </section>
 
         </div>
       </div>
