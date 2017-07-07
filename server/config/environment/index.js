58a422cae45e803b03fc69b83a2df1135a3a7f6e
monad98 <monad98@gmail.com>
2017-07-07 02:26:10 -0700

    facebook twitter login

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -37,7 +37,8 @@ const all = {
   facebook: {
     clientID: process.env.FACEBOOK_ID || 'id',
     clientSecret: process.env.FACEBOOK_SECRET || 'secret',
-    callbackURL: `${process.env.DOMAIN || ''}/auth/facebook/callback`
+    callbackURL: `${process.env.DOMAIN || ''}/auth/facebook/callback`,
+    callbackURLForAgent: `${process.env.DOMAIN || ''}/auth/facebook/agent/callback`,
   },
 
   twitter: {
